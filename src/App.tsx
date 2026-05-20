207a880eb055613b0534fe36b7853d17a546ce6c
Eliel Lucas <liel.lucas@gmail.com>
2026-05-20 12:17:15 -0300

    chore: remove unused useState import from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import CountryDetail from "./pages/CountryDetail/CountryDetail";
