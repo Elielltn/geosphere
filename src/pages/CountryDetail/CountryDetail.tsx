6c393e6140292cb376b5e0d394384e7e24893fc4
Eliel Lucas <126324244+Elielltn@users.noreply.github.com>
2026-05-27 21:44:14 -0300

    feat: add loading and error state for country details page

--- src/pages/CountryDetail/CountryDetail.tsx
+++ src/pages/CountryDetail/CountryDetail.tsx
@@ -1,3 +1,4 @@
+import styles from "./CountryDetail.module.css";
 import { useParams } from "react-router-dom";
 import DetailsHeader from "./components/DetailsHeader/DetailsHeader";
 import DetailsBody from "./components/DetailsBody/DetailsBody";
@@ -7,16 +8,23 @@ import type { typeCountryDetail } from "../../types/typeCountryDetail";
 function CountryDetail() {
   const { code } = useParams();
   const [data, setData] = useState<typeCountryDetail | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     async function fetchDetails() {
-      const [res1, res2] = await Promise.all([
-        fetch(`https://restcountries.com/v2/alpha/${code}`),
-        fetch(`https://servicodados.ibge.gov.br/api/v1/paises/${code}`),
-      ]);
-      const [json1, json2] = await Promise.all([res1.json(), res2.json()]);
-
-      setData({ ...json1, historico: json2[0]?.historico ?? "" });
+      try {
+        const [res1, res2] = await Promise.all([
+          fetch(`https://restcountries.com/v2/alpha/${code}`),
+          fetch(`https://servicodados.ibge.gov.br/api/v1/paises/${code}`),
+        ]);
+        const [json1, json2] = await Promise.all([res1.json(), res2.json()]);
+        setLoading(false);
+        setData({ ...json1, historico: json2[0]?.historico ?? "" });
+      } catch (err) {
+        setLoading(false);
+        setError("Não foi possível mostrar os dados do país. Tente novamente!");
+      }
     }
 
     if (code) fetchDetails();
@@ -24,8 +32,24 @@ function CountryDetail() {
 
   return (
     <>
-      <DetailsHeader countryName={data?.translations.pt ?? "..."} />
-      <DetailsBody data={data} />
+      <DetailsHeader
+        countryName={
+          error
+            ? "Erro"
+            : (data?.translations.pt ?? "Buscando os dados do país...")
+        }
+      />
+      {loading ? (
+        <div className={styles.feedbackContainer}>
+          <p>Carregando...</p>
+        </div>
+      ) : error ? (
+        <div className={styles.feedbackContainer}>
+          <p>{error}</p>
+        </div>
+      ) : (
+        <DetailsBody data={data} />
+      )}
     </>
   );
 }
